8064c48a3271b3c62d38860c35078f0fa823d0d6
Thiago Vieira <thiagovieira2609@gmail.com>
2021-02-05 00:18:56 -0300

    Tentativa de StyleSheet

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Layout from '../components/Layout/Layout';
 
+const prefix:string = '/portfolio';
+
 const IndexPage = () => (
   <div>
-    <link rel="stylesheet" href="/globals.css" />
+    <link rel="stylesheet" href={prefix + "/globals.css"} />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <Layout />
   </div>
