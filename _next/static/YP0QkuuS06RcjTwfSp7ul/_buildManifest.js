fdc13b9820d4025a270668f886da78c281bb167f
tvieira99 <53927816+tvieira99@users.noreply.github.com>
2021-02-06 00:51:24 +0000

    Deploying to gh-pages from  @ 82070c03db7e775cf5ee8e5cd25af14ae29bf03e 🚀

--- _next/static/YP0QkuuS06RcjTwfSp7ul/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6aa7bfebcbceb6c4e52e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4c6d028ab9783579d295.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
