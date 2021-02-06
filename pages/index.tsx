8bd91084e4ef400474a3b21e3e5a899b0643e886
Thiago Vieira <thiagovieira2609@gmail.com>
2021-02-05 21:46:50 -0300

    adicionando HEAD ao index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import Layout from '../components/Layout/Layout';
+import Head from 'next/head';
 
 const prefix:string = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 const IndexPage = () => (
   <div>
-    <link rel="stylesheet" href={prefix + "/globals.css"} />
+    <Head>
+      <title>Meu currículo</title>
+      <link rel="stylesheet" href={prefix + "/globals.css"} />
+    </Head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <Layout />
   </div>
